38ed02778886ad2edff42e1d332aafad52375148
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-12-11 09:20:40 -0700

    Seeding works. Started get all route for products. It doesnt work.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
-// Tags belongToMany Products (through ProductTag)
+// // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+  as: "product_tags"
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+  as: "product_tags"
+});
 
 module.exports = {
   Product,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll();
+    // (req.params, {
+    //   include: [{model: Category}, {model: Tag, through: ProductTag, as: "the_products"}]
+    // });
+    res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 // get one product
